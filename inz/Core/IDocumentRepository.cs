5ea002c0072a3fbb434d2626e0cbe85c26d4c933
jan.rebis <jrebis94@gmail.com>
2026-04-30 17:02:25 +0200

    Adds UpdateDocumentMetadataAsync method signature

--- inz/Core/IDocumentRepository.cs
+++ inz/Core/IDocumentRepository.cs
@@ -8,5 +8,6 @@ namespace inz.Service
         public Task AddDocumentMetadata(DocumentMetadata documentMetadata);
         public Task UpdateMetadataProcessingStatus(Guid documentMetadataId, ProcessStatus processingStatus);
         public Task<DocumentMetadata?> GetMetadaById(Guid documentMetadataId);
+        public Task UpdateDocumentMetadataAsync(Guid documentMetadataId, DocumentMetadata documentMetadata);
     }
 }
