177ddc4d0c8aa95a02364ad1ebdce2c34f766b28
jan.rebis <jrebis94@gmail.com>
2026-05-01 14:36:58 +0200

    feat(document): introduce CreateDocumentCommand for document creation

---
+++ inz/Core/CreateDocumentCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace inz.Core
+{
+    public sealed class CreateDocumentCommand
+    {
+        public IFormFile File { get; init; } = default!;
+        public string OwnerId { get; init; } = string.Empty;
+        public int OrganizationId { get; init; }
+    }
+}
