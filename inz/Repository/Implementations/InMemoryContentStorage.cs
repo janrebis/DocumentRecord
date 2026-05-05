9c539e67f04de8f917032f27e846b91e8cbe4a51
jan.rebis <jrebis94@gmail.com>
2026-05-05 21:18:20 +0200

    feat: - add InMemortContentStorage - add InMemoryDocumentRepository

---
+++ inz/Repository/Implementations/InMemoryContentStorage.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection.Metadata;
+using inz.Models;
+using inz.Repository.Interface;
+
+namespace inz.Repository.Implementations
+{
+    public class InMemoryContentStorage : IDocumentContentStorage
+
+    {
+        private readonly Dictionary<string, IFormFile> _inMemoryContentStorage = new();
+
+        public Task<string> AddDocumentToStorageAsync(IFormFile file)
+        {
+            string id = Guid.NewGuid().ToString();
+            _inMemoryContentStorage.Add(id, file);
+            return Task.FromResult(id);
+        }
+
+        public Task DeleteAsync(string blobKey)
+        {
+            _inMemoryContentStorage.Remove(blobKey);
+            return Task.CompletedTask;
+        }
+
+        public Task<bool> ExistsAsync(string blobKey)
+        {
+            var result = _inMemoryContentStorage.ContainsKey(blobKey);
+            return Task.FromResult(result);
+        }
+
+        public Task<Stream> GetDocumentStreamAsync(string blobKey)
+        {
+            var file = _inMemoryContentStorage[blobKey].OpenReadStream();
+            return Task.FromResult(file);
+        }
+
+        public Task UpdateDocumentInStorageAsync(string blobKey, IFormFile file)
+        {
+            _inMemoryContentStorage[blobKey] = file;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ inz/Repository/Implementations/InMemoryDocumentRepository.cs
@@ -0,0 +1,37 @@
+﻿using inz.Models;
+using inz.Models.Enums;
+using inz.Repository.Interface;
+
+namespace inz.Repository.Implementations
+{
+    public class InMemoryDocumentRepository : IDocumentRepository
+
+    {
+        private readonly Dictionary<int, DocumentMetadata> _inMemoryDocumentDatabase = new();
+        int idIncrementator = 1;
+        public Task AddDocumentMetadataAsync(DocumentMetadata documentMetadata)
+        {
+            _inMemoryDocumentDatabase.Add(idIncrementator, documentMetadata);
+            idIncrementator++;
+            return Task.CompletedTask;
+        }
+
+        public Task<DocumentMetadata?> GetMetadataByIdAsync(int documentMetadataId)
+        {
+            _inMemoryDocumentDatabase.TryGetValue(documentMetadataId, out var document);
+            return Task.FromResult(document);
+        }
+
+        public Task UpdateDocumentMetadataAsync(int documentMetadataId, DocumentMetadata documentMetadata)
+        {
+            _inMemoryDocumentDatabase[documentMetadataId] = documentMetadata;
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateMetadataProcessingStatusAsync(int documentMetadataId, ProcessStatus processingStatus)
+        {
+            _inMemoryDocumentDatabase.Remove(documentMetadataId);
+            return Task.CompletedTask;
+        }
+    }
+}
